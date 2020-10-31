37a0cff71b751aa1446d6fe58c12a2165671440d
Hirutsu <56251321+Hirutsu@users.noreply.github.com>
2020-10-31 18:41:24 +0400

    Update v.1.1.1
    Добавлено:
    Значок отображения в панели задач

--- CalculatorOnWinForm/Form1.Designer.cs
+++ CalculatorOnWinForm/Form1.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ButPanel));
             this.SettingPanel = new System.Windows.Forms.Panel();
+            this.MainName = new System.Windows.Forms.Label();
             this.ButChangeBG = new System.Windows.Forms.PictureBox();
             this.ButRollUp = new System.Windows.Forms.PictureBox();
             this.CloseButton = new System.Windows.Forms.PictureBox();
@@ -53,7 +55,7 @@
             this.ButPoint = new System.Windows.Forms.Button();
             this.ButEraseAll = new System.Windows.Forms.Button();
             this.LinePanel = new System.Windows.Forms.PictureBox();
-            this.MainName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SettingPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ButChangeBG)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ButRollUp)).BeginInit();
@@ -74,6 +76,17 @@
             this.SettingPanel.TabIndex = 1;
             this.SettingPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ButEraseAll_MouseDown);
             this.SettingPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ButEraseAll_MouseMove);
+            // 
+            // MainName
+            // 
+            this.MainName.AutoSize = true;
+            this.MainName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainName.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.MainName.Location = new System.Drawing.Point(8, 9);
+            this.MainName.Name = "MainName";
+            this.MainName.Size = new System.Drawing.Size(173, 24);
+            this.MainName.TabIndex = 3;
+            this.MainName.Text = "CalculatorByHirutsu";
             // 
             // ButChangeBG
             // 
@@ -117,7 +130,7 @@
             this.But7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.But7.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.But7.ForeColor = System.Drawing.Color.Gray;
-            this.But7.Location = new System.Drawing.Point(14, 370);
+            this.But7.Location = new System.Drawing.Point(12, 388);
             this.But7.Name = "But7";
             this.But7.Size = new System.Drawing.Size(82, 80);
             this.But7.TabIndex = 3;
@@ -155,7 +168,7 @@
             this.But8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.But8.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.But8.ForeColor = System.Drawing.Color.Gray;
-            this.But8.Location = new System.Drawing.Point(100, 370);
+            this.But8.Location = new System.Drawing.Point(98, 388);
             this.But8.Name = "But8";
             this.But8.Size = new System.Drawing.Size(82, 80);
             this.But8.TabIndex = 21;
@@ -172,7 +185,7 @@
             this.But9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.But9.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.But9.ForeColor = System.Drawing.Color.Gray;
-            this.But9.Location = new System.Drawing.Point(186, 370);
+            this.But9.Location = new System.Drawing.Point(184, 388);
             this.But9.Name = "But9";
             this.But9.Size = new System.Drawing.Size(82, 80);
             this.But9.TabIndex = 22;
@@ -189,7 +202,7 @@
             this.But6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.But6.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.But6.ForeColor = System.Drawing.Color.Gray;
-            this.But6.Location = new System.Drawing.Point(188, 284);
+            this.But6.Location = new System.Drawing.Point(186, 302);
             this.But6.Name = "But6";
             this.But6.Size = new System.Drawing.Size(82, 80);
             this.But6.TabIndex = 23;
@@ -206,7 +219,7 @@
             this.But3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.But3.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.But3.ForeColor = System.Drawing.Color.Gray;
-            this.But3.Location = new System.Drawing.Point(186, 198);
+            this.But3.Location = new System.Drawing.Point(184, 216);
             this.But3.Name = "But3";
             this.But3.Size = new System.Drawing.Size(82, 80);
             this.But3.TabIndex = 24;
@@ -223,7 +236,7 @@
             this.But2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.But2.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.But2.ForeColor = System.Drawing.Color.Gray;
-            this.But2.Location = new System.Drawing.Point(100, 198);
+            this.But2.Location = new System.Drawing.Point(98, 216);
             this.But2.Name = "But2";
             this.But2.Size = new System.Drawing.Size(82, 80);
             this.But2.TabIndex = 25;
@@ -240,7 +253,7 @@
             this.But5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.But5.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.But5.ForeColor = System.Drawing.Color.Gray;
-            this.But5.Location = new System.Drawing.Point(100, 284);
+            this.But5.Location = new System.Drawing.Point(98, 302);
             this.But5.Name = "But5";
             this.But5.Size = new System.Drawing.Size(82, 80);
             this.But5.TabIndex = 26;
@@ -257,7 +270,7 @@
             this.But1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.But1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.But1.ForeColor = System.Drawing.Color.Gray;
-            this.But1.Location = new System.Drawing.Point(12, 198);
+            this.But1.Location = new System.Drawing.Point(10, 216);
             this.But1.Name = "But1";
             this.But1.Size = new System.Drawing.Size(82, 80);
             this.But1.TabIndex = 27;
@@ -274,7 +287,7 @@
             this.But4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.But4.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.But4.ForeColor = System.Drawing.Color.Gray;
-            this.But4.Location = new System.Drawing.Point(14, 284);
+            this.But4.Location = new System.Drawing.Point(12, 302);
             this.But4.Name = "But4";
             this.But4.Size = new System.Drawing.Size(82, 80);
             this.But4.TabIndex = 28;
@@ -291,7 +304,7 @@
             this.ButPercent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ButPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ButPercent.ForeColor = System.Drawing.Color.Gray;
-            this.ButPercent.Location = new System.Drawing.Point(190, 112);
+            this.ButPercent.Location = new System.Drawing.Point(188, 130);
             this.ButPercent.Name = "ButPercent";
             this.ButPercent.Size = new System.Drawing.Size(82, 80);
             this.ButPercent.TabIndex = 29;
@@ -308,7 +321,7 @@
             this.ButtonErase.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ButtonErase.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ButtonErase.ForeColor = System.Drawing.Color.Gray;
-            this.ButtonErase.Location = new System.Drawing.Point(14, 112);
+            this.ButtonErase.Location = new System.Drawing.Point(12, 130);
             this.ButtonErase.Name = "ButtonErase";
             this.ButtonErase.Size = new System.Drawing.Size(82, 80);
             this.ButtonErase.TabIndex = 30;
@@ -325,7 +338,7 @@
             this.ButMultiplication.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ButMultiplication.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ButMultiplication.ForeColor = System.Drawing.Color.Gray;
-            this.ButMultiplication.Location = new System.Drawing.Point(274, 198);
+            this.ButMultiplication.Location = new System.Drawing.Point(272, 216);
             this.ButMultiplication.Name = "ButMultiplication";
             this.ButMultiplication.Size = new System.Drawing.Size(82, 80);
             this.ButMultiplication.TabIndex = 31;
@@ -344,7 +357,7 @@
             this.BubSubtraction.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BubSubtraction.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.BubSubtraction.ForeColor = System.Drawing.Color.Gray;
-            this.BubSubtraction.Location = new System.Drawing.Point(272, 284);
+            this.BubSubtraction.Location = new System.Drawing.Point(270, 302);
             this.BubSubtraction.Name = "BubSubtraction";
             this.BubSubtraction.Size = new System.Drawing.Size(82, 80);
             this.BubSubtraction.TabIndex = 32;
@@ -361,7 +374,7 @@
             this.ButAdditions.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ButAdditions.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ButAdditions.ForeColor = System.Drawing.Color.Gray;
-            this.ButAdditions.Location = new System.Drawing.Point(272, 370);
+            this.ButAdditions.Location = new System.Drawing.Point(270, 388);
             this.ButAdditions.Name = "ButAdditions";
             this.ButAdditions.Size = new System.Drawing.Size(82, 80);
             this.ButAdditions.TabIndex = 33;
@@ -378,7 +391,7 @@
             this.ButEqually.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ButEqually.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ButEqually.ForeColor = System.Drawing.Color.Gray;
-            this.ButEqually.Location = new System.Drawing.Point(272, 456);
+            this.ButEqually.Location = new System.Drawing.Point(270, 474);
             this.ButEqually.Name = "ButEqually";
             this.ButEqually.Size = new System.Drawing.Size(82, 80);
             this.ButEqually.TabIndex = 34;
@@ -395,7 +408,7 @@
             this.ButDivision.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ButDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ButDivision.ForeColor = System.Drawing.Color.Gray;
-            this.ButDivision.Location = new System.Drawing.Point(272, 112);
+            this.ButDivision.Location = new System.Drawing.Point(270, 130);
             this.ButDivision.Name = "ButDivision";
             this.ButDivision.Size = new System.Drawing.Size(82, 80);
             this.ButDivision.TabIndex = 35;
@@ -413,7 +426,7 @@
             this.But0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.But0.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.But0.ForeColor = System.Drawing.Color.Gray;
-            this.But0.Location = new System.Drawing.Point(14, 456);
+            this.But0.Location = new System.Drawing.Point(12, 474);
             this.But0.Name = "But0";
             this.But0.Size = new System.Drawing.Size(170, 80);
             this.But0.TabIndex = 37;
@@ -431,7 +444,7 @@
             this.ButPoint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ButPoint.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ButPoint.ForeColor = System.Drawing.Color.Gray;
-            this.ButPoint.Location = new System.Drawing.Point(184, 456);
+            this.ButPoint.Location = new System.Drawing.Point(182, 474);
             this.ButPoint.Name = "ButPoint";
             this.ButPoint.Size = new System.Drawing.Size(82, 80);
             this.ButPoint.TabIndex = 38;
@@ -449,7 +462,7 @@
             this.ButEraseAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ButEraseAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 29F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ButEraseAll.ForeColor = System.Drawing.Color.Gray;
-            this.ButEraseAll.Location = new System.Drawing.Point(102, 112);
+            this.ButEraseAll.Location = new System.Drawing.Point(100, 130);
             this.ButEraseAll.Name = "ButEraseAll";
             this.ButEraseAll.Size = new System.Drawing.Size(82, 80);
             this.ButEraseAll.TabIndex = 39;
@@ -471,23 +484,23 @@
             this.LinePanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ButEraseAll_MouseDown);
             this.LinePanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ButEraseAll_MouseMove);
             // 
-            // MainName
+            // textBox1
             // 
-            this.MainName.AutoSize = true;
-            this.MainName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.MainName.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.MainName.Location = new System.Drawing.Point(8, 9);
-            this.MainName.Name = "MainName";
-            this.MainName.Size = new System.Drawing.Size(173, 24);
-            this.MainName.TabIndex = 3;
-            this.MainName.Text = "CalculatorByHirutsu";
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(35)))), ((int)(((byte)(39)))));
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(0, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(367, 29);
+            this.textBox1.TabIndex = 40;
             // 
             // ButPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(35)))), ((int)(((byte)(39)))));
-            this.ClientSize = new System.Drawing.Size(367, 543);
+            this.ClientSize = new System.Drawing.Size(367, 565);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.ButEraseAll);
             this.Controls.Add(this.ButPoint);
             this.Controls.Add(this.But0);
@@ -511,6 +524,7 @@
             this.Controls.Add(this.LinePanel);
             this.Controls.Add(this.SettingPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ButPanel";
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ButEraseAll_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ButEraseAll_MouseMove);
@@ -552,6 +566,7 @@
         private System.Windows.Forms.Button ButEraseAll;
         private System.Windows.Forms.PictureBox ButChangeBG;
         private System.Windows.Forms.Label MainName;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
